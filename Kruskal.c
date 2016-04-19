d320c33f46f255243d007d255e6c209716c29e40
lanchenn <allielc@udel.edu>
2016-04-18 20:08:11 -0400

    Revised Version

--- Kruskal.c
+++ Kruskal.c
@@ -32,6 +32,13 @@ struct Graph
     struct Edge* edge;
 };
 
+// A structure to represent a subset for union-find
+struct subset
+{
+    int parent;
+    int rank;
+};
+
 // Creates a graph with V vertices and E edges
 struct Graph* createGraph(int V, int E)
 {
@@ -44,46 +51,11 @@ struct Graph* createGraph(int V, int E)
     return graph;
 }
 
-// A structure to represent a subset for union-find
-struct subset
-{
-    int parent;
-    int rank;
-};
+// Function prototypes for union-find (These functions are defined
+// after KruskalMST() method.
+int find (struct subset subsets[], int i);
+void Union(struct subset subsets[], int x, int y);
 
-// A utility function to find set of an element i
-// (uses path compression technique)
-int find(struct subset subsets[], int i)
-{
-    // find root and make root as parent of i (path compression)
-    if (subsets[i].parent != i)
-        subsets[i].parent = find(subsets, subsets[i].parent);
-    
-    return subsets[i].parent;
-}
-
-// A function that does union of two sets of x and y
-// (uses union by rank)
-void Union(struct subset subsets[], int x, int y)
-{
-    int xroot = find(subsets, x);
-    int yroot = find(subsets, y);
-    
-    // Attach smaller rank tree under root of high rank tree
-    // (Union by Rank)
-    if (subsets[xroot].rank < subsets[yroot].rank)
-        subsets[xroot].parent = yroot;
-    else if (subsets[xroot].rank > subsets[yroot].rank)
-        subsets[yroot].parent = xroot;
-    
-    // If ranks are same, then make one as root and increment
-    // its rank by one
-    else
-    {
-        subsets[yroot].parent = xroot;
-        subsets[xroot].rank++;
-    }
-}
 
 // Compare two edges according to their weights.
 // Used in qsort() for sorting an array of edges
@@ -145,6 +117,55 @@ void KruskalMST(struct Graph* graph)
                result[i].weight);
     return;
 }
+
+
+// A utility function to find set of an element i
+// (uses path compression technique)
+int find(struct subset subsets[], int i)
+{
+    // find root and make root as parent of i (path compression)
+    if (subsets[i].parent != i)
+        subsets[i].parent = find(subsets, subsets[i].parent);
+    
+    return subsets[i].parent;
+}
+
+// A function that does union of two sets of x and y
+// (uses union by rank)
+void Union(struct subset subsets[], int x, int y)
+{
+    int xroot = find(subsets, x);
+    int yroot = find(subsets, y);
+    
+    // Attach smaller rank tree under root of high rank tree
+    // (Union by Rank)
+    if (subsets[xroot].rank < subsets[yroot].rank)
+        subsets[xroot].parent = yroot;
+    else if (subsets[xroot].rank > subsets[yroot].rank)
+        subsets[yroot].parent = xroot;
+    
+    // If ranks are same, then make one as root and increment
+    // its rank by one
+    else
+    {
+        subsets[yroot].parent = xroot;
+        subsets[xroot].rank++;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+//---------------------------------------
 
 // Driver program to test above functions
 int main()
